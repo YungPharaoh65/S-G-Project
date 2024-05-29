05d87704f67918ddf1b29d28df3ec46acaeb9096
YungPharaoh65 <princemsimango27@gmail.com>
2024-05-29 23:19:31 +0200

    made change in Profile.jsx

--- src/Route-Navbar/Profile.jsx
+++ src/Route-Navbar/Profile.jsx
@@ -60,7 +60,7 @@ const Profile = () => {
   return (
     <div className={styles.profileContainer}>
       <Navbar />
-      <h1>Admin ID: {adminId}</h1>
+      <h1>Admin ID: #99999 {adminId}</h1>
 
       <div className={styles.box}>
         <br />
