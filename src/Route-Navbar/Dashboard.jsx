ba76d3c3dcd33e782e965c169332952b52e0f728
YungPharaoh65 <princemsimango27@gmail.com>
2024-06-01 21:10:43 +0200

    added a few things on Dashboard

--- src/Route-Navbar/Dashboard.jsx
+++ src/Route-Navbar/Dashboard.jsx
@@ -8,33 +8,44 @@ const Dashboard = () => {
     <h1>Dashboard</h1>
 
     <div className={styles.graphbox2}>
-        <p className={styles.graphtext}>Total Sales</p>
-        <p className={styles.graphnumber}>R 205 453</p>
+        <p className={styles.graphtext}>Ready For Shipment</p>
+        <p className={styles.graphnumber}>132 lbs  |  06pcs</p>
+        <hr />
         <p className={styles.graphtext}>over last week</p>
       </div>
 
       <div className={styles.graphbox3}>
-        <p className={styles.graphtext}>Total Sales</p>
-        <p className={styles.graphnumber}>R 205 453</p>
+        <p className={styles.graphtext}>Avg. Transit Time</p>
+        <p className={styles.graphnumber}>2 DAYS air  |  5 DAY/S ship</p>
+        <hr />
         <p className={styles.graphtext}>over last week</p>
       </div>
 
       <div className={styles.graphbox4}>
-        <p className={styles.graphtext}>Total Sales</p>
+        <p className={styles.graphtext}>Set price</p>
         <p className={styles.graphnumber}>R 205 453</p>
         <p className={styles.graphtext}>over last week</p>
       </div>
 
       <div className={styles.graphbox5}>
-        <p className={styles.graphtext}>Total Sales</p>
-        <p className={styles.graphnumber}>R 205 453</p>
+        <p className={styles.graphtext}>Order Frequency</p>
+        <p className={styles.graphnumber}>Friday, 10 May 2024</p>
         <p className={styles.graphtext}>over last week</p>
       </div>
 <br /><br />
 
-      <div className={styles.vertbox}>hgfhg</div>
-      <br />
-      <br />
+      <div className={styles.vertbox}>
+        <div className={styles.graphheader}>Order list Database</div> <div className={styles.date}>Tuesday, 14 May 2024</div>
+        
+        <div className={styles.movebtn}>
+        <button className={styles.pendingshipments}>Pending Shipment</button>
+
+        <button className={styles.downloadingreports}>Download Reports</button>
+        <button className={styles.createshipment}>Create Shipment</button>
+        </div>
+        </div>
+
+     
 
       <div className={styles.graphbox44}>
         <p className={styles.graphtext}>Total Sales</p>
@@ -43,7 +54,7 @@ const Dashboard = () => {
       </div>
 
       <div className={styles.graphbox45}>
-        <p className={styles.graphtext}>Total Sales</p>
+        <p className={styles.graphtext}>Most Shipped Goods</p>
         <p className={styles.graphnumber}>R 205 453</p>
         <p className={styles.graphtext}>over last week</p>
       </div>
@@ -53,7 +64,6 @@ const Dashboard = () => {
         <p className={styles.graphnumber}>R 205 453</p>
         <p className={styles.graphtext}>over last week</p>
       </div>
-
 
     </div>
   )
